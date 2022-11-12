48d0fd5288083924db7c56a2a920ce991aa2bde0
Dillon Baird <dillonbaird@outlook.com>
2022-11-11 19:04:12 -0800

    Fix when theres no options defined

--- src/index.js
+++ src/index.js
@@ -27,13 +27,25 @@ export function imgBanner(imgSrc, message) {
 
 export function styleAll(options) {
     console.log = function (...args) {
-        var t = [
-            (options.bgColor ? "background-color: " + options.bgColor : "background-color: red"),
-            (options.padding ? "padding: " + options.padding : "padding: 10px 20px"),
-            (options.lineHeight ? "line-height: " + options.lineHeight : "line-height: 120px"),
-            (options.fontSize ? "font-size: " + options.fontSize : "font-size: 20px"),
-            (options.fontFamily ? "font-family: " + options.fontFamily : "font-family: monospace")
-        ].join(";")
+        if(options){
+            var t = [
+                (options.bgColor ? "background-color: " + options.bgColor : "background-color: red"),
+                (options.padding ? "padding: " + options.padding : "padding: 10px 20px"),
+                (options.lineHeight ? "line-height: " + options.lineHeight : "line-height: 120px"),
+                (options.fontSize ? "font-size: " + options.fontSize : "font-size: 20px"),
+                (options.fontFamily ? "font-family: " + options.fontFamily : "font-family: monospace")
+            ].join(";")
+        }
+        else {
+            var t = [
+                "background-color: #fff",
+                "padding: 10px 20px",
+                "line-height: 120px",
+                "font-size: 60px",
+                "font-family: monospace",
+            ].join(";");
+        }
+        
         console.info("%c" + args, t)
     };
 }
